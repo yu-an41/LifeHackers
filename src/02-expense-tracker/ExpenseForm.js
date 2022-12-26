f6aca4842c657fdb54e893b0f81b2d71542e2e28
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-12-26 13:00:25 +0800

    add states for inputs

--- src/02-expense-tracker/ExpenseForm.js
+++ src/02-expense-tracker/ExpenseForm.js
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from './ExpenseForm.module.scss'
 
 export default function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState('')
+  const [enteredAmount, setEnteredAmount] = useState('')
+  const [enteredDate, setEnteredDate] = useState('')
+
+  const titleChangeHandler = (e) => {
+    setEnteredTitle(e.target.value)
+  }
+
+  const amountChangeHandler = (e) => {
+    setEnteredAmount(e.target.value)
+  }
+  const dateChangeHandler = (e) => {
+    setEnteredDate(e.target.value)
+  }
   return (
     <form>
       <div className={styles.newExpenseControls}>
         <div className={styles.newExpenseControl}>
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className={styles.newExpenseControl}>
           <label>Amount</label>
-          <input type="number" min="1" step="1" />
+          <input
+            type="number"
+            min="1"
+            step="1"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className={styles.newExpenseControl}>
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className={styles.newExpenseActions}>
