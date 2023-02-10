5f5d9d2819af2db794b26ec38e2b9f505cabaf49
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-02-10 23:47:08 +0800

    edit addExpenseHandler()

--- src/02-expense-tracker/ExpenseTracker.js
+++ src/02-expense-tracker/ExpenseTracker.js
@@ -7,18 +7,23 @@ import ExpenseCard from './ExpenseCard'
 import ExpensesFilter from './ExpensesFilter'
 import NewExpense from './NewExpense'
 
+const DUMMY_EXPENSE = [
+  { title: 'Fried rice', date: new Date(2021, 12, 23), amount: 75 },
+  { title: 'Bubble tea', date: new Date(2022, 12, 22), amount: 80 },
+  { title: 'Bubble tea', date: new Date(2022, 12, 21), amount: 105 },
+  { title: 'Bubble tea', date: new Date(2022, 12, 20), amount: 50 },
+]
+
 function ExpenseTracker() {
-  const expenses = [
-    { title: 'Fried rice', date: new Date(2021, 12, 23), amount: 75 },
-    { title: 'Bubble tea', date: new Date(2022, 12, 22), amount: 80 },
-    { title: 'Bubble tea', date: new Date(2022, 12, 21), amount: 105 },
-    { title: 'Bubble tea', date: new Date(2022, 12, 20), amount: 50 },
-  ]
-  const addExpenseHandler = (expense) => {
-    console.log(expenses)
-  }
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSE)
 
   const [filteredYear, setFilteredYear] = useState('2022')
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [...prevExpenses, expense]
+    })
+  }
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear)
